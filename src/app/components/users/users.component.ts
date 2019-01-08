0e2b9fee12bd630ea5f404be63adef086c4ba1c7
danchamorro <dan@danchamorro.com>
2019-01-08 16:15:11 -0500

    Added Bootstrap

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor() {}
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Chris',
+        lastName: 'Mike',
+        age: 22,
+        address: {
+          street: '05 Main st',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Eric',
+        lastName: 'Smith',
+        age: 40,
+        address: {
+          street: '150 Main st',
+          city: 'Boston',
+          state: 'MA'
+        }
+      }
+    ];
   }
-
 }

ca7a44a538ce097fbb2b097e3bfee63940e6d1dc
danchamorro <dan@danchamorro.com>
2019-01-09 19:09:38 -0500

    Refactored users component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -15,15 +15,15 @@ export class UserComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.user = {
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 30,
-      address: {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
-      }
-    };
+    // this.user = {
+    //   firstName: 'John',
+    //   lastName: 'Doe',
+    //   age: 30,
+    //   address: {
+    //     street: '50 Main st',
+    //     city: 'Boston',
+    //     state: 'MA'
+    //   }
+    // };
   }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,12 +11,7 @@ export class UsersComponent implements OnInit {
   user: User = {
     firstName: '',
     lastName: '',
-    age: null,
-    address: {
-      street: '',
-      city: '',
-      state: ''
-    }
+    email: ''
   };
   users: User[];
   showExtended: boolean = true;
@@ -31,12 +26,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'John',
         lastName: 'Doe',
-        age: 70,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'MA'
-        },
+        email: 'test@test.com',
         isActive: true,
         registered: new Date('01/02/2018 08:30:00'),
         hide: true
@@ -44,12 +34,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'Kevin',
         lastName: 'Johnson',
-        age: 34,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'MA'
-        },
+        email: 'test@test.com',
         isActive: false,
         registered: new Date('03/11/2017 06:20:00'),
         hide: true
@@ -57,12 +42,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'Karen',
         lastName: 'Williams',
-        age: 26,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'FL'
-        },
+        email: 'test@test.com',
         isActive: true,
         registered: new Date('11/02/2016 10:30:00'),
         hide: true
@@ -72,21 +52,16 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  addUser() {
-    this.users.unshift(this.user);
+  // addUser() {
+  //   this.users.unshift(this.user);
 
-    // This will reset form to blank values
-    this.user = {
-      firstName: '',
-      lastName: '',
-      age: null,
-      address: {
-        street: '',
-        city: '',
-        state: ''
-      }
-    };
-  }
+  //   // This will reset form to blank values
+  //   this.user = {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: ''
+  //   };
+  // }
 
   onSubmit(e) {
     e.preventDefault();
